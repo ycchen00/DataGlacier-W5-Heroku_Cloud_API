0f36311874ae4eef462ad6b037fb7dc650a6f407
cyc <48916942+ycchen00@users.noreply.github.com>
2022-10-19 00:30:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
+
 @app.route('/')
 def home():
     return render_template('index.html')
